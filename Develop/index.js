cf7a6e7eaec3b65bc0a96c5b67b870a85d13f460
BurnsD <Dustinburnsactech@gmail.com>
2022-01-22 23:30:48 -0600

    test for gen

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,30 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [
+    // GitHub Username
+    {
+        type: 'input',
+        message: "What is your GitHub username?",
+        name: 'username'
+    },
+    // GitHub Repo
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repo?",
+        name: 'repo',
+    },
+    // Email
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is an Email people can contact you at about this project?'
+    },
     // Title
     {
         type: 'input',
@@ -26,20 +44,14 @@ const questions = [
     { 
         type: 'input',
         name: 'installation',
-        message: 'Describe the steps required to install this project or skip.'
+        message: 'What does the user need to install to run this app (ie...dependencies)? (Skip with "Enter")'
 
     },
     // Usage
     {
         type: 'input',
         name: 'usage',
-        message: 'Provide instructions and examples for use.'
-    },
-    // Credits
-    {
-        type: 'input',
-        name: 'credits',
-        message: 'List your collaborators, if any. Along with any third-party assets or tutorials followed.'
+        message: 'How is the app used? Give instructions'
     },
     // License 
     {
@@ -47,19 +59,6 @@ const questions = [
         name: 'license',
         message: 'What license would you like to use?',
         choices: ['MIT', 'APACHE 2.0', 'ISC', 'Mozilla', 'Academic', 'GNU', 'Open']
-    },
-    // Badges
-    {
-        type: 'input',
-        name: 'badges',
-        message: ''
-    },
-    // Features
-    {
-        type: 'input',
-        name: 'features',
-        message: 'If your project has a lot of features, add some description to them here.',
-        
     },
     // Contributing
     {
@@ -71,16 +70,16 @@ const questions = [
     {
         type: 'input',
         name: 'test',
-        message: 'If you have written any tests, provide examples on how to run them here.'
+        message: 'What commands are needed to test this app?'
     },
 ];
 
-// TODO: Create a function to write README file
+// Writes README file
 function writeToFile(fileName, data) {
     fs.writeFileSync(path.join(process.cwd(), fileName), data)
-}
+};
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
     inquirer.prompt(questions).then(answers => {
        const markdown = generateMarkdown(answers);
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,12 +1,19 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Returns a license badge based on which license is passed in
+// If there is no license, returns an empty string
 function renderLicenseBadge(license) {
-  // return '![license Image](./assets/img.png)'
-
+  //
+  if(license !== 'no license'){
+    return `
+    ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+      return ' ';
+    };
 };
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Returns the license link
+// If there is no license, returns an empty string
+
 function renderLicenseLink(license) {
   if (license !== 'no license'){
     return `
@@ -17,23 +24,56 @@ function renderLicenseLink(license) {
   };
 };
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// Returns the license section of README
+// If there is no license, returns an empty string
+
 function renderLicenseSection(license) {
-  if (license !== 'no license'){
-  return `## License
-The license for our project is a ${renderLicenselink(license)} license.
-  `;
+  if (license !== 'no license') {
+    return `
+  ## [License](#table-of-contents)
+  This application has the following license:
+  ${renderLicenseLink(license)}
+    `;
   } else {
     return ' ';
   }
-};
+}
 
-// TODO: Create a function to generate markdown for README
+// Generates markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-    ${renderLicenseLink(data.license)}
-`;
-};
+  return `
+  # ${data.title}
+  
+  ${renderLicenseBadge(data.license)}
+  
+  ## Table-of-Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ${renderLicenseLink(data.license)}
 
+  ## [Description]
+  ${data.description}
+   
+  ## [Installation](#table-of-contents)
+  ${data.installation}
+
+  ## [Usage](#table-of-contents)
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+  ## [Contributing](#table-of-contents)
+    
+   
+  ## [Tests](#table-of-contents)
+  ${data.test}
+  ## [Questions](#table-of-contents)
+  Contact me with any questions at the following:
+  [GitHub](https://github.com/${data.githubUsername})
+  [Email: ${data.email}](mailto:${data.email})
+  `;
+};
 module.exports = generateMarkdown;
