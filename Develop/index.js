ca283ceb726b9d47c0f74c42e1dd96f02c0b0da5
BurnsD <Dustinburnsactech@gmail.com>
2022-01-22 19:42:29 -0600

    added format for questions

--- Develop/index.js
+++ Develop/index.js
@@ -7,12 +7,58 @@ const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
+    // Title
     {
         type: 'input',
         name: 'title',
         message: 'what is the title of your project?'
 
-    }
+    },
+    
+    // Description
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Decribe this project.'
+
+    },
+    // Installation
+    { 
+        type: 'input',
+        name: 'installation',
+        message: 'Describe the steps required to install this project.'
+
+    },
+    // Usage
+    {
+        type: 'input',
+        name: 'usage',
+        message: ''
+    },
+    // Credits
+    {
+        type: 'input',
+        name: 'credits',
+        message: ''
+    },
+    // License 
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license would you like to use?',
+        choices: ['MIT', 'APACHE 2.0']
+    },
+    // Badges
+    {
+    
+    },
+    // Features
+    {
+        type: '',
+        name: '',
+        message: '',
+        
+    },
 ];
 
 // TODO: Create a function to write README file
